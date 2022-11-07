4d63f1a3c6f331ad95218972781cde9c7304ef49
Yiheng Duan <duan_y@worksap.co.jp>
2022-11-07 22:03:57 +0800

    PowerOfTwo

---
+++ src/main/java/leetcode/editor/en/PowerOfTwo.java
@@ -0,0 +1,58 @@
+package leetcode.editor.en;
+//Given an integer n, return true if it is a power of two. Otherwise, return 
+//false. 
+//
+// An integer n is a power of two, if there exists an integer x such that n == 2
+//ˣ. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: n = 1
+//Output: true
+//Explanation: 2⁰ = 1
+// 
+//
+// Example 2: 
+//
+// 
+//Input: n = 16
+//Output: true
+//Explanation: 2⁴ = 16
+// 
+//
+// Example 3: 
+//
+// 
+//Input: n = 3
+//Output: false
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// -2³¹ <= n <= 2³¹ - 1 
+// 
+//
+// 
+//Follow up: Could you solve it without loops/recursion?
+//
+// Related Topics Math Bit Manipulation Recursion 👍 4371 👎 329
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Solution solution = new PowerOfTwo().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isPowerOfTwo(int n) {
+            return n > 0 && (n & (n - 1)) == 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
